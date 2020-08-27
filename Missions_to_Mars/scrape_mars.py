c695c3c4da158deb1451015f6ccb4853af388451
samanthadevlin <64792728+samanthadevlin@users.noreply.github.com>
2020-08-27 19:43:08 -0400

    ah

--- Missions_to_Mars/scrape_mars.py
+++ Missions_to_Mars/scrape_mars.py
@@ -58,13 +58,61 @@ def scrape_image():
         #create bs object and parse w/ html parser
         soup = BeautifulSoup(response.text, 'html.parser')
         #print(soup.prettify())
-    try:
+    try: 
+
         relative_image_path = soup.find_all('img')[2]["src"]
         carousel_item = url + relative_image_path
         #print(carousel_item)
     except AttributeError:
         return None, None 
     
+# Mars Facts 
+def scrape_facts():
+    #url to be scraped
+    url = 'https://space-facts.com/mars/'
+    #retrive page w/ request module
+    response = requests.get(url)
+    browser.visit(url)
+    tables = pd.read_html(url)
+    mars_facts = tables[0]
+    mars_facts.columns = ['Description','Value']
+    mars_facts.to_html(header = False, index = False)
+    return mars_facts
+
+# Mars Hemispheres
+def scrape_hemispheres():
+    #url to be scraped
+    url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
+    #retrive page w/ request module
+    response = requests.get(url)
+    browser.visit(url)
+    #create bs object and parse w/ html parser
+    soup = BeautifulSoup(response.text, 'html.parser')
+    #print(soup.prettify())
+    #scrape  USGS Astrogeology 
+    hemisphere = soup.find_all('div', class_='item')
+    USGS_url = 'https://astrogeology.usgs.gov'
+
+    #loop through all images (reference activity 5 mongo scraping)
+for hemis in hemisphere:
+    #Store title
+    title = hemis.find("h3").text
+    # Store link of the image
+    temp_img = hemis.find('a', class_= 'itemLink product-item')['href']
+    
+    #visit the link
+    browser.visit(USGS_url + temp_img)
+    
+    temp_img_html = browser.html
+    
+    soup_img_html = BeautifulSoup(temp_img_html, 'html.parser')
+    
+    full_img_url = USGS_url + soup_img_html.find('img', class_='wide-image')['src']
+    
+    hemisphere_image_url.append({"title":title, "img_url":full_img_url})
    
+hemisphere_image_url
+
+
 
 
